94c51495f9035488772b1b61124994b12e9bfd0f
Omar Khairat <omaralkhairat@gmail.com>
2019-07-28 16:25:21 +0400

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybashutil',
-    version='0.1',
+    version='0.2',
     author='Omar Khairat',
     author_email='omaralkhairat@gmail.com',
     packages = find_packages(),
