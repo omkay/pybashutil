3d5a4952e9aaa8ae8016c441697d86cf15528db9
Omar Khairat <omaralkhairat@gmail.com>
2019-07-28 16:32:09 +0400

    fixed date ranges

--- pybashutil/yearmonthdayrun.py
+++ pybashutil/yearmonthdayrun.py
@@ -8,7 +8,7 @@ dates = sys.argv[1]
 
 for date in pybashutil.date_range(dates):
     args = [v.replace("__HDATE__", "{}-{}-{}".format(date[:4], date[4:6], date[6:])).replace("__DATE__", date) for v in sys.argv[2:]]
-    args = [v.replace("__HMONTH__", "{}".format(date[4:6])).replace("__MONTH__", date) for v in args]
-    args = [v.replace("__HYEAR__", "{}".format(date[:4])).replace("__YEAR__", date) for v in args]
+    args = [v.replace("__HMONTH__", "{}".format(date[4:6])).replace("__MONTH__", date[4:6]) for v in args]
+    args = [v.replace("__HYEAR__", "{}".format(date[:4])).replace("__YEAR__", date[:4]) for v in args]
     print(args)
     subprocess.check_call(args, stdout=sys.stdout, stderr=sys.stderr)
