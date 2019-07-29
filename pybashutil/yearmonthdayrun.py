7ef740576241a33c04b1d69a2c4da0fb97bbac96
Omar Khairat <omaralkhairat@gmail.com>
2019-07-29 22:54:21 +0400

    added day var to args

--- pybashutil/yearmonthdayrun.py
+++ pybashutil/yearmonthdayrun.py
@@ -10,5 +10,6 @@ for date in pybashutil.date_range(dates):
     args = [v.replace("__HDATE__", "{}-{}-{}".format(date[:4], date[4:6], date[6:])).replace("__DATE__", date) for v in sys.argv[2:]]
     args = [v.replace("__HMONTH__", "{}".format(date[4:6])).replace("__MONTH__", date[4:6]) for v in args]
     args = [v.replace("__HYEAR__", "{}".format(date[:4])).replace("__YEAR__", date[:4]) for v in args]
+    args = [v.replace("__HDAY__", "{}".format(date[6:])).replace("__DAY__", date[6:]) for v in args]
     print(args)
     subprocess.check_call(args, stdout=sys.stdout, stderr=sys.stderr)
